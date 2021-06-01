18206a88571c4d63eb59cc8df1a6bae36fff97fa
serramatutu <lucasvvop@hotmail.com>
2021-06-01 19:01:55 -0300

    add version command

---
+++ internal/config/build.go
@@ -0,0 +1,10 @@
+package config
+
+// This will be replaced using ldargs
+var (
+	Version    = "dev"
+	Commit     = "none"
+	Date       = "unknown"
+	BuiltBy    = "unknown"
+	Repository = "unknown"
+)
