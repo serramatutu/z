01d9aaf60c65abe38531b5516a926bf5de4ad3bf
serramatutu <lucasvvop@hotmail.com>
2021-06-01 00:12:50 -0300

    add arg schema type tests

--- internal/argparse/schema_test.go
+++ internal/argparse/schema_test.go
@@ -1,47 +1,139 @@
-package argparse_test
+package argparse
 
-// import (
-// 	"testing"
+import (
+	"reflect"
+	"regexp"
+	"testing"
+)
 
-// 	"github.com/serramatutu/z/internal/argparse"
-// )
+func TestParseStringArgumentNoPattern(t *testing.T) {
+	arg := stringArgument{
+		name:     "arg-name",
+		optional: false,
+	}
+	arg.Parse("value")
 
-// func TestParseRangeInvalid(t *testing.T) {
-// 	_, _, err := argparse.ParseRange("x:y")
-// 	if err == nil {
-// 		t.Errorf("Expected error for ParseRange with invalid arguments")
-// 	}
-// }
+	if arg.Err() != nil {
+		t.Errorf("stringArgument.Parse() with no pattern should not return error")
+	}
 
-// func TestParseRangeFull(t *testing.T) {
-// 	start, end, err := argparse.ParseRange("1:2")
-// 	if err != nil {
-// 		t.Errorf("Unexpected error for ParseRange")
-// 	}
+	if arg.Value() != "value" {
+		t.Errorf("Expected stringArgument without pattern to assume any value")
+	}
+}
 
-// 	if start != 1 || end != 2 {
-// 		t.Errorf("Expected 1:2 but got %v:%v for ParseRange", start, end)
-// 	}
-// }
+func TestParseStringArgumentWithPatternOk(t *testing.T) {
+	arg := stringArgument{
+		name:     "arg-name",
+		optional: false,
+		pattern:  "[A-z]+",
+	}
+	arg.Parse("Value")
 
-// func TestParseRangeStart(t *testing.T) {
-// 	start, end, err := argparse.ParseRange("1:")
-// 	if err != nil {
-// 		t.Errorf("Unexpected error for ParseRange")
-// 	}
+	if arg.Err() != nil {
+		t.Errorf("stringArgument.Parse() with matching pattern should not return error")
+	}
 
-// 	if start != 1 || end != 0 {
-// 		t.Errorf("Expected 1:0 but got %v:%v for ParseRange", start, end)
-// 	}
-// }
+	if arg.Value() != "Value" {
+		t.Errorf("Expected stringArgument with matching pattern to assume given value")
+	}
+}
 
-// func TestParseRangeEnd(t *testing.T) {
-// 	start, end, err := argparse.ParseRange(":5")
-// 	if err != nil {
-// 		t.Errorf("Unexpected error for ParseRange")
-// 	}
+func TestParseStringArgumentWithPatternNotWholeMatch(t *testing.T) {
+	arg := stringArgument{
+		name:     "arg-name",
+		optional: false,
+		pattern:  "[A-z]+",
+	}
+	arg.Parse("Value1234")
 
-// 	if start != 0 || end != 5 {
-// 		t.Errorf("Expected 0:5 but got %v:%v for ParseRange", start, end)
-// 	}
-// }
+	if arg.Err() == nil {
+		t.Errorf("stringArgument.Parse() with partially matching pattern should return error")
+	}
+}
+
+func TestParsePatternArgumentOk(t *testing.T) {
+	arg := patternArgument{
+		name:     "arg-name",
+		optional: false,
+	}
+	arg.Parse("[A-z]+")
+
+	if arg.Err() != nil {
+		t.Errorf("patternArgument.Parse() with compilable pattern should not return error")
+	}
+
+	expected := regexp.MustCompile("[A-z]+")
+	if !reflect.DeepEqual(*arg.Value(), *expected) {
+		t.Errorf("Expected patternArgument with compilable pattern to assume compiled regex value")
+	}
+}
+
+func TestParsePatternArgumentInvalidPattern(t *testing.T) {
+	arg := patternArgument{
+		name:     "arg-name",
+		optional: false,
+	}
+	arg.Parse("[A-z][")
+
+	if arg.Err() == nil {
+		t.Errorf("patternArgument.Parse() with invalid pattern should return error")
+	}
+}
+
+func TestRangeArgumentInvalid(t *testing.T) {
+	arg := rangeArgument{
+		name:     "arg-name",
+		optional: false,
+	}
+	arg.Parse("x:y")
+
+	if arg.Err() == nil {
+		t.Errorf("Expected error for rangeArgument.Parse() with invalid arguments")
+	}
+}
+
+func TestParseRangeFull(t *testing.T) {
+	arg := rangeArgument{
+		name:     "arg-name",
+		optional: false,
+	}
+	arg.Parse("1:2")
+	if arg.Err() != nil {
+		t.Errorf("Unexpected error for ParseRange")
+	}
+
+	if arg.Start() != 1 || arg.End() != 2 {
+		t.Errorf("Expected 1:2 but got %v:%v for ParseRange", arg.Start(), arg.End())
+	}
+}
+
+func TestParseRangeStart(t *testing.T) {
+	arg := rangeArgument{
+		name:     "arg-name",
+		optional: false,
+	}
+	arg.Parse("1:")
+	if arg.Err() != nil {
+		t.Errorf("Unexpected error for ParseRange")
+	}
+
+	if arg.Start() != 1 || arg.End() != 0 {
+		t.Errorf("Expected 1:0 but got %v:%v for ParseRange", arg.Start(), arg.End())
+	}
+}
+
+func TestParseRangeEnd(t *testing.T) {
+	arg := rangeArgument{
+		name:     "arg-name",
+		optional: false,
+	}
+	arg.Parse(":5")
+	if arg.Err() != nil {
+		t.Errorf("Unexpected error for ParseRange")
+	}
+
+	if arg.Start() != 0 || arg.End() != 5 {
+		t.Errorf("Expected 0:5 but got %v:%v for ParseRange", arg.Start(), arg.End())
+	}
+}
