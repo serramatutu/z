63e7fa1584dcdc3fdbe9d7193893f3a1d8e5ec33
serramatutu <lucasvvop@hotmail.com>
2021-06-03 21:28:22 -0300

    theme toggle

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -1,7 +1,26 @@
-// hamburger toggle
+"use strict";
+
+function setTheme(theme) {
+    localStorage.setItem("theme", theme);
+
+    let isDark = theme === "dark";
+    document.getElementById("theme-toggle").setAttribute("checked", isDark);
+
+    document.body.setAttribute("data-theme", theme)
+}
+
 window.addEventListener("load", (ev) => {
-    let toggleBtn = document.getElementById("sidebar-toggle-button");
-    toggleBtn.addEventListener("click", (ev) => {
+    let theme = localStorage.getItem("theme") || "light";
+    setTheme(theme);
+
+    let sidebarToggleBtn = document.getElementById("sidebar-toggle");
+    sidebarToggleBtn.addEventListener("click", (ev) => {
         document.getElementById("sidebar-container").classList.toggle("active");
+    });
+
+    let themeToggleBtn = document.getElementById("theme-toggle");
+    themeToggleBtn.addEventListener("click", (ev) => {
+        theme = ev.target.checked ? "dark" : "light";
+        setTheme(theme);
     });
 });
